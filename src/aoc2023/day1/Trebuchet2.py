ae76d15659e7a377ca6bfc91253c60c86f95f0f5
Katinka Slingerland <katinka.slingerland@sogeti.com>
2023-12-01 09:15:53 +0100

    sterf

--- src/aoc2023/day1/Trebuchet2.py
+++ src/aoc2023/day1/Trebuchet2.py
@@ -10,33 +10,51 @@ with open('input.txt') as f:
 [print(line.strip()) for line in contents]
 
 totaal = 0
+getallen={'one':1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6, 'seven':7, 'eight':8, 'nine':9};
 
 for x in contents:
-    lokaal = x.strip()
+    regel = x.strip()
     som=0
     beginInt = None
     eindInt = None
 
-    for element in lokaal:
+    gevondengetallen = {}
+
+    print(regel)
+
+    for getal in list(getallen.keys()):
+        index = regel.find(getal)
+        if index >-1:
+            gevondengetallen[index] = getallen[getal]
+            print(getal + ' gevonden in ' + regel + ': plek ' + str(index))
+
+    for num, element in enumerate(regel, start=0):
         som=0
         try:
             int(element)
-            print ('int ' + element)
-            if beginInt==None :
-                beginInt=element
-                print ('begin ='+ beginInt)
-            else :
-                eindInt=element
-                print('eind =' + eindInt)
+            print(element + ' gevonden in ' + regel + ': plek ' + str(num))
+            gevondengetallen[num] = int(element)
         except:
             pass
 
-    if eindInt == None and beginInt != None:
-        som = str(beginInt) + str(beginInt)
-        print('if '+som)
-    else:
-        som = str(beginInt) + str(eindInt)
-        print('else ' +som)
+    print(gevondengetallen)
+    keysList = list(gevondengetallen.keys())
+    keysList.sort()
+    # print(keysList)
+    # print('eerste')
+    # print(gevondengetallen[keysList[0]])
+    # print('laatste' + str(keysList[len(keysList)-1]))
+    # print(gevondengetallen[keysList[len(keysList)-1]])
+    som = str(gevondengetallen[keysList[0]]) + str(gevondengetallen[keysList[len(keysList)-1]])
+    print(som)
     totaal += int(som)
 
+    # if eindInt == None and beginInt != None:
+    #     som = str(beginInt) + str(beginInt)
+    #     print('if '+som)
+    # else:
+    #     som = str(beginInt) + str(eindInt)
+    #     print('else ' +som)
+    # totaal += int(som)
+    #
     print(f'totaal{totaal}')
